f2fe910ad02b8423b908493c234c5a91bcfbedb0
Thomas Venhorst <thomas.w.venhorst@gmail.com>
2022-03-07 15:18:23 +0100

    done

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam , achternaam, geboortedatum, woonplaats) 
+VALUES (8, 'cors' , 'veld' , '2000-18-07' , 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam , achternaam, geboortedatum, woonplaats)
+VALUES (9, 'peet' , 'meer' , '2004-06-9', 'Amsterdam')
+
+INSERT INTO studenten (student_id, voornaam , achternaam, geboortedatum, woonplaats) 
+VALUES (10, 'jack' , 'van meer' , '1999-21-2' , 'Amsterdam')
