35d38bb0637e6a57de2ae55a86b9f593cb34e0b6
Thomas Venhorst <thomas.w.venhorst@gmail.com>
2022-03-07 14:39:43 +0100

    done

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,17 @@
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (8, 'piet', 'vandam', '1A', 'straatnaam-a', '1198XY', '2004-01-18', 'Amsterdam');
 
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (9, 'kaan', 'van de zee', '1B', 'straatnaam-b', '1122AA', '2005-06-03', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (11, 'jaap', 'scherpings', '1C', 'straatnaam-c', '3190AX', '1999-04-09', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (12, 'piet', 'de groot', '1D', 'straatnaam-d', '8291ZA', '1997-06-12', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (13, 'lise', 'oog', '1F', 'straatnaam-e', '7931CV', '2000-02-11', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (14, 'cas', 'van peaken', '1G', 'straatnaam-f', '9374JF', '2006-06-09', 'Amsterdam'); 
